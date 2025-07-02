610ef6b2ca8c9c46e21961adf79464b1dc8af4ed
Alberto Florez <florez_alberto@hotmail.com>
2025-07-02 21:07:24 +0200

    dont mess with storage yet

--- start.sh
+++ start.sh
@@ -8,11 +8,6 @@ if [ ! -d "/data/ipfs/blocks" ]; then
   ipfs init
 fi
 
-# Set storage max from env, default to 10GB
-if [ -n "$IPFS_STORAGE_MAX" ]; then
-  echo "Setting IPFS storage limit to $IPFS_STORAGE_MAX"
-  ipfs config Datastore.StorageMax "$IPFS_STORAGE_MAX"
-fi
 
 # Start IPFS daemon
 echo "Starting IPFS daemon..."
